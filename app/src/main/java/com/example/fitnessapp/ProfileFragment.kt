5065905299e86c3c4c5d7ab5e8609cf2a9c8cd0e
killllerWhale <93337303+killllerWhale@users.noreply.github.com>
2023-03-23 17:20:32 +0300

    df

--- app/src/main/java/com/example/fitnessapp/ProfileFragment.kt
+++ app/src/main/java/com/example/fitnessapp/ProfileFragment.kt
@@ -1,21 +1,25 @@
 package com.example.fitnessapp
 
-import android.content.Intent
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
 import android.widget.ProgressBar
 import android.widget.TextView
+import com.example.fitnessapp.databinding.DialogWaterBinding
 import com.example.fitnessapp.databinding.FragmentProfileBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import java.text.SimpleDateFormat
 import java.util.*
 
 class ProfileFragment : Fragment() {
 
     lateinit var binding: FragmentProfileBinding
+    lateinit var bindingDialog : DialogWaterBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,14 +35,31 @@ class ProfileFragment : Fragment() {
         updateProgressBar()
         //присваиваем актуальную дату
         updateDateText()
-        val goSettings = binding.goSettings
-        goSettings.setOnClickListener {
+
+        binding.goSettings.setOnClickListener {
             val newFragment = SettingsFragment()
             val transaction = parentFragmentManager.beginTransaction()
             transaction.replace(R.id.container, newFragment)
             transaction.addToBackStack(null)
             transaction.commit()
         }
+
+        binding.buttonGoWater.setOnClickListener {
+            val context = requireContext()
+            val dialog = Dialog(context)
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+            dialog.setContentView(R.layout.dialog_water)
+            dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            dialog.show()
+            bindingDialog = DialogWaterBinding.inflate(layoutInflater)
+            bindingDialog.waterGlass2.setOnClickListener {
+                bindingDialog.waterGlass2.setBackgroundResource(R.drawable.icon_water)
+            }
+        }
+    }
+
+    private fun updateStatusWater(){
+
     }
 
     private fun updateProgressBar() {
