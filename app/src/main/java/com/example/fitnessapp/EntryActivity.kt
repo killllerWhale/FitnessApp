a14389f546e3d7f33d3219228d87e44e010ce032
Ruslan <114252945+OsmanovRuslan@users.noreply.github.com>
2023-03-23 15:57:18 +0300

    Новые кнопки

--- app/src/main/java/com/example/fitnessapp/EntryActivity.kt
+++ app/src/main/java/com/example/fitnessapp/EntryActivity.kt
@@ -4,23 +4,24 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.Fragment
 import com.example.fitnessapp.databinding.ActivityEntryBinding
 
 class EntryActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityEntryBinding
+    private lateinit var binding: ActivityEntryBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEntryBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.enter.setOnClickListener{
-            startActivity(Intent(this, MainActivity::class.java))
-        }
-        binding.go.setOnClickListener{
-            startActivity(Intent(this, MainActivity::class.java))
-        }
+        loadFragment(EntryFragment())
 
+    }
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.commit()
     }
 }
---
+++ app/src/main/java/com/example/fitnessapp/EntryFragment.kt
@@ -0,0 +1,33 @@
+package com.example.fitnessapp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.fitnessapp.databinding.FragmentEntryBinding
+
+class EntryFragment : Fragment() {
+
+    private lateinit var binding: FragmentEntryBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentEntryBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.go.setOnClickListener{
+            startActivity(Intent(activity, MainActivity::class.java))
+        }
+        binding.enter.setOnClickListener{
+            startActivity(Intent(activity, MainActivity::class.java))
+        }
+    }
+}
