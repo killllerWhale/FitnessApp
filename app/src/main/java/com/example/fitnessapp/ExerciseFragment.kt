3b1700633a54baf51483e4d7330c1f3fad38fbac
Ruslan <114252945+OsmanovRuslan@users.noreply.github.com>
2023-03-25 13:08:08 +0300

    Упражнение

---
+++ app/src/main/java/com/example/fitnessapp/ExerciseFragment.kt
@@ -0,0 +1,28 @@
+package com.example.fitnessapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.fitnessapp.databinding.FragmentExerciseBinding
+
+class ExerciseFragment : Fragment() {
+
+    private lateinit var binding: FragmentExerciseBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentExerciseBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+}
